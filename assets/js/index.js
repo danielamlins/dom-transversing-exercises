200d1e3c323c8d84742ed837a62bce136a963b94
Daniela Lins <danielamlins@gmail.com>
2020-12-03 11:37:04 +0100

    added task1

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// Select the header that is the closest to the first h1 heading. Give the header a solid border of 5px.
+let h1 = document.querySelector("h1");
+let header = h1.closest("header");
+header.style.border = "5px solid black";
+
+
