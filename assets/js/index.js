0e918fdd833113dc1e1bbc6a718d92eda98fed77
Daniela Lins <danielamlins@gmail.com>
2020-12-03 11:40:34 +0100

    added task2

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,10 @@ let header = h1.closest("header");
 header.style.border = "5px solid black";
 
 
+// If the .info section contains an .info-package, select all package-titles and give the title's previous element a border
+let info = document.querySelector(".info");
+let infoPackage = document.querySelector(".info-package");
+if (info.contains(infoPackage)){
+    let packageTitle = document.querySelectorAll(".package-title");
+    packageTitle.forEach(el => el.previousElementSibling.style.border = "2px solid black");
+}
