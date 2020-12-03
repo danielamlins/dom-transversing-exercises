a07c9054334ec7f5b0b5557b9d485538ca43edf3
Daniela Lins <danielamlins@gmail.com>
2020-12-03 13:20:24 +0100

    restyled navbar, headers and footers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 // Select the header that is the closest to the first h1 heading. Give the header a solid border of 5px.
 let h1 = document.querySelector("h1");
 let header = h1.closest("header");
-header.style.border = "5px solid black";
+// header.style.border = "5px solid black";
 
 
 // If the .info section contains an .info-package, select all package-titles and give the title's previous element a border
@@ -9,18 +9,18 @@ let info = document.querySelector(".info");
 let infoPackage = document.querySelector(".info-package");
 if (info.contains(infoPackage)){
     let packageTitle = document.querySelectorAll(".package-title");
-    packageTitle.forEach(el => el.previousElementSibling.style.border = "2px solid black");
+    packageTitle.forEach(el => el.previousElementSibling.style.border = "1px solid black");
 }
 
 // Check if the label's class matches "mild". If so, give the label a yellow solid border. If the label's class matches "intense", give the label an orange solid border. If the class does not match either, give the label a red solid border.
 let labels = document.querySelectorAll("label");
 labels.forEach(el => {
     if (el.matches(".mild")){
-        el.style.borderBottom = "2px solid yellow";
+        el.style.borderBottom = "4px solid yellow";
     } else if (el.matches(".intense")){
-        el.style.borderBottom = "2px solid orange";
+        el.style.borderBottom = "4px solid orange";
     } else {
-        el.style.borderBottom = "2px solid red";
+        el.style.borderBottom = "4px solid red";
     }
 });
 
